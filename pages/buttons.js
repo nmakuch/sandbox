b3ef5647066019e743bcd1a90a7f170301a16507
nmakuch <makuch.nick@gmail.com>
2022-02-17 14:32:51 -0500

    button page description

--- pages/buttons.js
+++ pages/buttons.js
@@ -6,7 +6,7 @@ export default function Home() {
         <div>
             <Head>
                 <title>Buttons</title>
-                <meta name="description" content="Testing a skip to content component" />
+                <meta name="description" content="A space for testing Canada.ca buttons" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
