76ff2e8c8115036bc8bcc8c8f585eb8b052bd46a
nmakuch <makuch.nick@gmail.com>
2022-03-02 20:32:03 -0500

    skip

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ef68af32f2207c28"; }
+/******/ 		__webpack_require__.h = function() { return "8e1f0b1479066d2d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/skip.js
+++ pages/skip.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Skip.module.css'
 
-export default function Buttons() {
+export default function Skip() {
     return (
         <div>
             <Head>
